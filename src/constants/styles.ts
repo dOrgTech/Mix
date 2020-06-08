4d94b273a1179299ef961173f686bcd131c8be24
Nestor Amesty <nestor09amesty@gmail.com>
2020-06-08 14:48:02 +0200

    [WIP] Landing Page styling

--- src/constants/styles.ts
+++ src/constants/styles.ts
@@ -1,5 +1,9 @@
 export const StyleConstants = {
   backgroundColor: '#171E2C',
   textColor: '#E3E4E5',
-  borderPrimary: '1px solid #2C3340'
+  borderPrimary: '1px solid #2C3340',
+  borderButton: '1px solid #325661',
+  borderButtonHover: '1px solid #437880',
+  backgroundButtonHover: '#1A2532',
+  gray: '#7D8189'
 }
